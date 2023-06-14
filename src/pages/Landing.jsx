a0a080c54ec478d82d11f644d1a1c49794316fbd
Bartw00 <wojtanbartosz@gmail.com>
2023-06-14 17:29:02 +0200

    landing - inline styles replaced

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,6 +5,26 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Layout from "../features/landing/components/Layout";
 import { Box, Button } from "@mui/material";
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { css } from "@mui/material/styles";
+
+
+const BoxStyle = css({
+  color: "#fff", 
+  height: "auto", 
+  overflow: "hidden"
+});
+
+const typographyStyleBody1 = css({
+  marginTop: "24px"
+});
+
+const buttonStyle = css({
+  padding: "16px",
+  marginTop: "24px",
+  marginBottom: "24px",
+  width: "40%",
+});
+
 
 const Landing = () => {
   const { isAuthenticated } = useAuth0();
@@ -31,23 +51,18 @@ const Landing = () => {
           buttons={["play", "repeat", "frame", "debug"]}
         />
       </Player>
-      <Box sx={{ color: "#fff", height: "auto", overflow: "hidden" }}>
+      <Box sx={BoxStyle}>
         <Typography variant="h2" component="h1">
           This is the best instagram desktop app you can imagine!
         </Typography>
-        <Typography variant="body1" sx={{ marginTop: "24px" }}>
+        <Typography variant="body1" sx={typographyStyleBody1}>
           Log in and see by yourself
         </Typography>
 
         <Button
           onClick={loginWithRedirect}
           variant="contained"
-          sx={{
-            padding: "16px",
-            marginTop: "24px",
-            marginBottom: "24px",
-            width: "40%",
-          }}
+          sx={buttonStyle}
         >
           Log in
         </Button>
